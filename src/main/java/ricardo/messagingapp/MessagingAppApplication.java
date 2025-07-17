a46e97f5dd40e7ebc60d1b00fda6b724efd8b654
Ricardo <ricardomorim05@gmail.com>
2025-07-17 22:59:56 +0100

    feat: add dotenv support and configuration for environment variables

--- src/main/java/ricardo/messagingapp/MessagingAppApplication.java
+++ src/main/java/ricardo/messagingapp/MessagingAppApplication.java
@@ -1,5 +1,6 @@
 package ricardo.messagingapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MessagingAppApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(MessagingAppApplication.class, args);
     }
 
